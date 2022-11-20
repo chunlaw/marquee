0af648c570173d49525ac1b62d8e9c5652ec86ed
Law Wai Chun <chunlaw@rocketmail.com>
2022-11-20 13:18:40 +0800

    Change tab title

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Marquee from "./Marquee";
 import Panel from "./Panel";
 import "./i18n";
+import { useTranslation } from "react-i18next";
 
 function App() {
+  const { t } = useTranslation()
+
+  useEffect(() => {
+    document.title = t('App Name')
+  }, [t])
+
   return (
     <Routes>
       <Route path="/:text" element={<Marquee />} />
